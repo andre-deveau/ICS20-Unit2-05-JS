da86b9be57b2f18693b494d9fc3654eac630a2a6
Andre <113055933+andre-deveau@users.noreply.github.com>
2023-01-09 14:27:33 -0500

    init

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 Andre Deveau All rights reserved
+//
+// Created by: Andre Deveau
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates pay and income tax
+ */
+function enterClicked() {
+  //input
+  const hours = parseInt(document.getElementById("hours-worked").value)
+  const rate = parseInt(document.getElementById("hourly-rate").value)
+
+  //process
+  const takeHomeSalary = hours * rate * (1.0 - 0.18)
+  const INCOMETAX = hours * rate * 0.18
+
+  //output
+  document.getElementById("pay").innerHTML =
+    "Your pay will be: $" + takeHomeSalary.toFixed(2)
+  document.getElementById("income-tax").innerHTML =
+    "The government will take: $" + INCOMETAX.toFixed(2)
+}
