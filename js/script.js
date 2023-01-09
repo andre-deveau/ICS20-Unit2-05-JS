4e87a647133f9eb10a3bf69de78efcc206dcc203
andre-deveau <andre-deveau@users.noreply.github.com>
2023-01-09 19:29:39 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,23 +4,23 @@
 // Created on: Oct 2022
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 /**
  * This function calculates pay and income tax
  */
 function enterClicked() {
   //input
-  const hours = parseInt(document.getElementById("hours-worked").value)
-  const rate = parseInt(document.getElementById("hourly-rate").value)
+  const hours = parseInt(document.getElementById("hours-worked").value);
+  const rate = parseInt(document.getElementById("hourly-rate").value);
 
   //process
-  const takeHomeSalary = hours * rate * (1.0 - 0.18)
-  const INCOMETAX = hours * rate * 0.18
+  const takeHomeSalary = hours * rate * (1.0 - 0.18);
+  const INCOMETAX = hours * rate * 0.18;
 
   //output
   document.getElementById("pay").innerHTML =
-    "Your pay will be: $" + takeHomeSalary.toFixed(2)
+    "Your pay will be: $" + takeHomeSalary.toFixed(2);
   document.getElementById("income-tax").innerHTML =
-    "The government will take: $" + INCOMETAX.toFixed(2)
+    "The government will take: $" + INCOMETAX.toFixed(2);
 }
